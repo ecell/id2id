ed68481081e8ec6ad51458c9927ed9d3825dea40
Kozo Nishida <kozo.nishida@gmail.com>
2022-04-30 19:29:03 +0900

    Update bigg2kegg.py

--- bigg2kegg.py
+++ bigg2kegg.py
@@ -1,5 +1,5 @@
-# !wget http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt
-
 import pandas as pd
-a = pd.read_table("http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt")
-a[a['database_links'].str.contains("http://identifiers.org/kegg.reaction", na=False)].to_csv("bigg2kegg.csv")
+df = pd.read_table("http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt")
+df['database_links'] = df['database_links'].str.split('; ')
+df = df.explode('database_links')
+df.head()
