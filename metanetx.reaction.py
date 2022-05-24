c1ac9f8bf3da35470bb28821183bfc04165c46c7
Kozo Nishida <kozo.nishida@gmail.com>
2022-05-24 22:25:56 +0900

    Update metanetx.reaction.py

--- metanetx.reaction.py
+++ metanetx.reaction.py
@@ -1,2 +1,16 @@
 import pandas as pd
 df = pd.read_table("./reac_xref.tsv", skiprows=351)
+df2 = df.drop(columns=['description'])
+df3 = df2['#source'].str.split(':', expand=True)
+
+idorgpref = []
+for i in requests.get("https://registry.api.identifiers.org/restApi/namespaces?size=9999").json()['_embedded']['namespaces']:
+    idorgpref.append(i['prefix'])
+    
+df4=df3[df3[0].isin(idorgpref)]
+df5=df2[df3[0].isin(idorgpref)]
+
+result = pd.DataFrame({'idenfiersorg1': df4[0], 'id1': df4[1], 'id2': df5['ID']})
+result['idenfiersorg2'] = 'metanetx.reaction'
+result = result[['idenfiersorg1', 'id1', 'idenfiersorg2', 'id2']]
+result.to_csv('metanetx.reaction.csv', index=False)
