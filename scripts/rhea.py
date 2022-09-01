2d2b49b2524e1db8e7c08cb3a0b3812bbb14e367
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-02 01:46:30 +0900

    Update rhea.py

--- scripts/rhea.py
+++ scripts/rhea.py
@@ -1,7 +1,7 @@
 import pandas as pd
 df = pd.read_csv('rhea2xrefs.tsv', sep='\t')
 df.drop(['DIRECTION', 'MASTER_ID'], axis=1, inplace=True)
-tmp = df['DB'].map({'EC': 'ec-code', 'GO': 'GO', 'METACYC': 'metacyc.reaction', 'KEGG_REACTION': 'kegg.reaction', 'REACTOME': 'reactome', 'MACIE': 'macie'})
+tmp = df['DB'].map({'EC': 'ec-code', 'GO': 'GO', 'METACYC': 'metacyc.reaction', 'KEGG_REACTION': 'kegg.reaction', 'ECOCYC': 'biocyc', 'REACTOME': 'reactome', 'MACIE': 'macie'})
 df['DB'] = tmp
 tmp = df['ID'].str.replace('^GO:', '', regex=True)
 df['ID'] = tmp
