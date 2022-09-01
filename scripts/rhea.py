002ce4da9b630ff1b5bf6bf7bbd4acbe179b72d7
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-01 15:00:55 +0900

    Create rhea.py

---
+++ scripts/rhea.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv('rhea2xrefs.tsv', sep='\t')
+df.drop(['DIRECTION', 'MASTER_ID'], axis=1, inplace=True)
+tmp = df['DB'].map({'EC': 'ec-code', 'GO': 'GO', 'METACYC': 'metacyc.reaction', 'KEGG_REACTION': 'kegg.reaction', 'REACTOME': 'reactome', 'MACIE': 'macie'})
+df['DB'] = tmp
+tmp = df['ID'].str.replace('^GO:', '', regex=True)
+df['ID'] = tmp
+df['identifiersorgprefix1'] = "rhea"
+df.rename(columns = {'RHEA_ID':'id1', 'ID':'id2', 'DB':'identifiersorgprefix2'}, inplace = True)
