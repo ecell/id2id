8a4a454766c452704dc9a782af38ddabc7d55fca
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-02 02:09:56 +0900

    Add rhea (#14)
    * Rename download.sh to wgetdata.sh
    
    * Update wgetdata.sh
    
    * Create rhea.py
    
    * Update rhea.py
    
    * Update rhea.py
    
    * Update rhea.py

--- scripts/wgetdata.sh
+++ scripts/wgetdata.sh
@@ -1,3 +1,4 @@
 wget http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt
 wget http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt
 wget https://www.metanetx.org/cgi-bin/mnxget/mnxref/reac_xref.tsv
+wget https://ftp.expasy.org/databases/rhea/tsv/rhea2xrefs.tsv
