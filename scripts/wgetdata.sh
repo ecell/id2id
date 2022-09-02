c963fd378d0379ff14ab16f09a3ef0c547c7608a
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-02 19:05:09 +0900

    Update wgetdata.sh

--- scripts/wgetdata.sh
+++ scripts/wgetdata.sh
@@ -2,3 +2,4 @@ wget http://bigg.ucsd.edu/static/namespace/bigg_models_metabolites.txt
 wget http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt
 wget https://www.metanetx.org/cgi-bin/mnxget/mnxref/reac_xref.tsv
 wget https://ftp.expasy.org/databases/rhea/tsv/rhea2xrefs.tsv
+wget https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/database_accession_3star.tsv
