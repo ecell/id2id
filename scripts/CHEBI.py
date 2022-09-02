52683d85163a1c70804b66f4937702f273cf624a
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-02 19:12:01 +0900

    Create CHEBI.py

---
+++ scripts/CHEBI.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('compound_origins_3star.tsv', sep='\t')
+df.drop(['ID', 'SOURCE'], axis=1, inplace=True)
+thisdata2identifiersorg = {"KEGG COMPOUND accession":"kegg.compound",
+                           "CAS Registry Number":"cas",
+                           "GlyTouCan accession":"glytoucan",
