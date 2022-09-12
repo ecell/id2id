143d1cf03fd1bf68fdbb2ec493d7a7cfa43a50d3
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-13 00:06:13 +0900

    Update CHEBI.py

--- opendata/CHEBI.py
+++ opendata/CHEBI.py
@@ -1,6 +1,7 @@
 import pandas as pd
 df = pd.read_csv('compound_origins_3star.tsv', sep='\t')
 df.drop(['ID', 'SOURCE'], axis=1, inplace=True)
+
 thisdata2identifiersorg = {"KEGG COMPOUND accession":"kegg.compound",
                            "CAS Registry Number":"cas",
                            "GlyTouCan accession":"glytoucan",
@@ -12,4 +13,9 @@ thisdata2identifiersorg = {"KEGG COMPOUND accession":"kegg.compound",
                            "UM-BBD compID":"umbbd.compound",
                            "Beilstein Registry Number":None,
                            "LIPID MAPS instance accession":"lipidmaps",
-                           "KEGG DRUG accession":"kegg.drug",}
+                           "KEGG DRUG accession":"kegg.drug",
+                           "Gmelin Registry Number":None,
+                           "PDBeChem accession":"pdb-ccd",
+                           "KEGG GLYCAN accession":"kegg.glycan",
+                           "Wikipeida accession":"wikipedia.en",
+                           "DrugBank accession":"drugbank",}
