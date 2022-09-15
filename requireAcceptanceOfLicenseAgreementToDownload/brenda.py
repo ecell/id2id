39b583e989d5e0105a9b6b61427a65fbfafdc56b
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-16 04:50:31 +0900

    Add BRENDA (#18)
    * Create wgetdata.sh
    
    * Update pubchem.compound.py
    
    * Create brenda.py
    
    * Update brenda.py

---
+++ requireAcceptanceOfLicenseAgreementToDownload/brenda.py
@@ -0,0 +1,23 @@
+import json
+
+brenda=json.load(open("brenda_2022_2.json"))
+
+data = brenda['data']
+ecnumbers = data.keys()
+
+f = open('brenda.csv', 'w')
+for ecnumber in c:
+    if ecnumber != 'spontaneous':
+        if 'proteins' in b[ecnumber]:
+            proteins = b[ecnumber]['proteins']
+            for protein in proteins.values():
+                # if len(protein) > 1:
+                #     print(protein)
+                # print(protein[0])
+                for accession in protein[0]['accessions']:
+                    # if protein[0]['source'] != 'uniprot':
+                    #     print(accession)
+                    #     print(protein[0]['source'])
+                    # print('brenda,' + ecnumber + ',' + protein[0]['source'] + ',' + accession)
+                    f.write('brenda,' + ecnumber + ',' + protein[0]['source'] + ',' + accession + '\n')
+f.close()
