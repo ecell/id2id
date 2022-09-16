26b0ec69b98bcdcbde022f59bce62c596b60ba75
Kozo Nishida <kozo.nishida@gmail.com>
2022-09-17 02:34:28 +0900

    Make folder division fuzzy

--- opendata/wgetdata.sh
+++ opendata/wgetdata.sh
@@ -3,3 +3,4 @@ wget http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt
 wget https://www.metanetx.org/cgi-bin/mnxget/mnxref/reac_xref.tsv
 wget https://ftp.expasy.org/databases/rhea/tsv/rhea2xrefs.tsv
 wget https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/database_accession_3star.tsv
+wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/ECOLI_83333_idmapping.dat.gz
