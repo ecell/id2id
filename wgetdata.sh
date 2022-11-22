af300f83ed7cf546e25fc9ccef3c86e4bfe6a371
Kozo Nishida <kozo.nishida@gmail.com>
2022-11-22 17:17:43 +0900

    Update wgetdata.sh

--- wgetdata.sh
+++ wgetdata.sh
@@ -4,4 +4,4 @@ wget https://www.metanetx.org/cgi-bin/mnxget/mnxref/reac_xref.tsv
 wget https://ftp.expasy.org/databases/rhea/tsv/rhea2xrefs.tsv
 wget https://ftp.ebi.ac.uk/pub/databases/chebi/Flat_file_tab_delimited/database_accession_3star.tsv
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/ECOLI_83333_idmapping.dat.gz
-wget https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-InChI-Key.gz
+#wget https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-InChI-Key.gz
